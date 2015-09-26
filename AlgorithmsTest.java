5932b46cf9ea34210bb07b7837da6d32d6971fef
acorll.was@gmail.com <HakAl@users.noreply.github.com>
2015-09-26 08:32:05 -0400

    benchmark

--- AlgorithmsTest.java
+++ AlgorithmsTest.java
@@ -1,24 +1,109 @@
 public class AlgorithmsTest
 {
+    static final int ITERATIONS = 1000000;
 
     public static void main(String[] args)
     {
-        exerciseEuclid();
+        System.out.println("results for 10,000 iterations on euclid(): " + runBenchmark(10000));
+        System.out.println("results for 10,000 iterations on euclidOne(): " + runBenchmarkA(10000));
+        System.out.println("results for 10,000 iterations on euclidTwo(): " + runBenchmarkB(10000));
     }
 
-    static void exerciseEuclid()
+    /**
+     * @param iterations
+     * @return average time
+     */
+    private static long runBenchmark(int iterations)
     {
-        testEuclid(55, 12222);
-        testEuclid(39485784, 324340);
-        testEuclid(551, 119);
-        testEuclid(200, 400);
-        testEuclid(544, 119);
-        testEuclid(119, 544);
+        long[] times = new long[iterations];
+        for (int i = 0; i < iterations; i++) {
+            times[i] = benchmark();
+        }
+        long sum = 0;
+        for (long time :
+                times) {
+            sum += time;
+        }
+        return sum / iterations;
+    }
+
+    private static long runBenchmarkA(int iterations)
+    {
+        long[] times = new long[iterations];
+        for (int i = 0; i < iterations; i++) {
+            times[i] = benchmarkA();
+        }
+        long sum = 0;
+        for (long time :
+                times) {
+            sum += time;
+        }
+        return sum / iterations;
+    }
+
+    private static long runBenchmarkB(int iterations)
+    {
+        long[] times = new long[iterations];
+        for (int i = 0; i < iterations; i++) {
+            times[i] = benchmarkB();
+        }
+        long sum = 0;
+        for (long time :
+                times) {
+            sum += time;
+        }
+        return sum / iterations;
+    }
+
+
+    static long benchmark()
+    {
+        long start = System.nanoTime();
+        int i = 0;
+        do {
+            Algorithms.euclid(544, 119);
+            i++;
+        } while (i < ITERATIONS);
+        return System.nanoTime() - start;
+    }
+
+    static long benchmarkA()
+    {
+        long start = System.nanoTime();
+        int i = 0;
+        do {
+            Algorithms.euclidOne(544, 119);
+            i++;
+        } while (i < ITERATIONS);
+        return System.nanoTime() - start;
+    }
+
+    static long benchmarkB()
+    {
+        long start = System.nanoTime();
+        int i = 0;
+        do {
+            Algorithms.euclidTwo(544, 119);
+            i++;
+        } while (i < ITERATIONS);
+        return System.nanoTime() - start;
     }
 
     static void testEuclid(int m, int n)
     {
         System.out.println(String.format(
-                "Euclids result for m = %1$s, n = %2$s is: %3$s.", m, n, Algorithms.euclid(m, n)));
+                "Euclid result for m = %1$s, n = %2$s is: %3$s.", m, n, Algorithms.euclid(m, n)));
+    }
+
+    static void testEuclidOne(int m, int n)
+    {
+        System.out.println(String.format(
+                "EuclidOne result for m = %1$s, n = %2$s is: %3$s.", m, n, Algorithms.euclidOne(m, n)));
+    }
+
+    static void testEuclidTwo(int m, int n)
+    {
+        System.out.println(String.format(
+                "EuclidTwo result for m = %1$s, n = %2$s is: %3$s.", m, n, Algorithms.euclidTwo(m, n)));
     }
 }
